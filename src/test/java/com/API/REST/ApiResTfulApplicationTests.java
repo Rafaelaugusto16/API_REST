28f79824ea94b649d1a35f76ec738672a7b514a8
Rafael Augusto <Rafaelaugusto.alves16@gmail.com>
2023-10-13 18:26:12 -0300

    Criando uma API Restful

---
+++ src/test/java/com/API/REST/ApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
